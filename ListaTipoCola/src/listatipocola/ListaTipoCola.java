9723639acc1864e2940b2d6dd9da629d4bd52145
LuisIvan2021 <92408803+LuisIvan2021@users.noreply.github.com>
2022-11-14 12:00:37 -0600

    Add files via upload

---
+++ ListaTipoCola/src/listatipocola/ListaTipoCola.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listatipocola;
+
+/**
+ *
+ * @author ivan_
+ */
+public class ListaTipoCola {
+
+    public static void main(String[] args) {
+    
+    }
+    
+}
